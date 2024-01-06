fee87d8bbb8af0b6024b766c7cb7ddf2bc2931c4
samir suroshe <sameersuroshe50@gmail.com>
2024-01-06 11:46:05 +0530

    Project  setup

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from 'cors'
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+// database name --> youtubedb
+
+// this use for cross origin sharing 
+app.use(cors({ origin: process.env.CORS_ORIGIN}))
+// this middleware use for parsing the json data
+app.use(express.json({limit:"16kb"}))
+// this is used for parsing url data extended is used for nessted object
+app.use(express.urlencoded({extended: true}))
+// this is used for accessing public resources from server
+app.use(express.static("public"))
+// this is used to parse the cookie
+app.use(cookieParser());
+
+export default app
